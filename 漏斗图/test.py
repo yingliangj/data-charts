201e19b1461f7d23a3fac186b0880acc0df2a34f
ariescc <jiachang@mail.nwpu.edu.cn>
2018-01-29 15:33:26 +0800

    漏斗图

---
+++ 漏斗图/test.py
@@ -0,0 +1,9 @@
+from pyecharts import Funnel
+
+attr = ['衬衫', '羊毛衫', '雪纺衫', '裤子', '高跟鞋', '袜子']
+value = [20, 40, 60, 80, 100, 120]
+funnel = Funnel('漏斗图')
+
+funnel.add('商品', attr, value, is_label_show=True, label_pos='inside', label_text_color='#fff')
+funnel.render()
+
